02bf0523364d83ed56e4bd02c2bc4c789f75581b
Muzamil <Muzamilb174@gmail.com>
2023-12-07 00:09:04 +0530

    Update arrays.cpp

--- Programs/ArrayRepresentation/arrays.cpp
+++ Programs/ArrayRepresentation/arrays.cpp
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -11,7 +12,7 @@ int main() {
 
 
     int* array = malloc(20 * sizeof(int));
-
+    
     // Checking storage 
     // int array: {1,2,3,4,5,6,7,8,9,10,};
 
